deec1254cf461706502688562e35ce9ec3ed8be0
Pranav A <pranav.avva@gmail.com>
2017-09-05 17:41:49 -0400

    Fixed up Helper class

--- src/main/java/me/pavva/PokeJava/App.java
+++ src/main/java/me/pavva/PokeJava/App.java
@@ -4,12 +4,11 @@ import java.util.Scanner; // User input
 import java.util.Random; // Random numbers
 import java.util.ArrayList;
 
-import me.pavva.PokeJava.moves.*;
+// import me.pavva.PokeJava.moves.*;
 import me.pavva.PokeJava.pokemon.*;
 
-/**
- * me.pavva.PokeJava.App
- * 
+@SuppressWarnings("unused")
+/** 
  * Entry point to Pokemon Battle Simulator
  * 
  * @author  Pranav Avva   Github: 20avva    Web: <a href="https://20avva.github.io">20avva.github.io</a>
@@ -18,6 +17,7 @@ import me.pavva.PokeJava.pokemon.*;
  * @see me.pavva.PokeJava.Pokemon
  * @see me.pavva.PokeJava.Type
  * @see me.pavva.PokeJava.Move
+ * @see me.pavva.PokeJava.Helper
  */
 public class App {
 
@@ -41,35 +41,35 @@ public class App {
     private static final WaterPokemon    water                  = new WaterPokemon();
 
 
-    private static final String          ANSI_RESET             = "\u001B[0m";             // ANSI Color Codes
-    private static final String          ANSI_BLACK             = "\u001B[30m";
-    private static final String          ANSI_RED               = "\u001B[31m";
-    private static final String          ANSI_GREEN             = "\u001B[32m";
-    private static final String          ANSI_YELLOW            = "\u001B[33m";
-    private static final String          ANSI_BLUE              = "\u001B[34m";
-    private static final String          ANSI_PURPLE            = "\u001B[35m";
-    private static final String          ANSI_CYAN              = "\u001B[36m";
-    private static final String          ANSI_WHITE             = "\u001B[37m";
+    static final String                  ANSI_RESET             = "\u001B[0m";             // ANSI Color Codes
+    static final String                  ANSI_BLACK             = "\u001B[30m";
+    static final String                  ANSI_RED               = "\u001B[31m";
+    static final String                  ANSI_GREEN             = "\u001B[32m";
+    static final String                  ANSI_YELLOW            = "\u001B[33m";
+    static final String                  ANSI_BLUE              = "\u001B[34m";
+    static final String                  ANSI_PURPLE            = "\u001B[35m";
+    static final String                  ANSI_CYAN              = "\u001B[36m";
+    static final String                  ANSI_WHITE             = "\u001B[37m";
 
-    private static final String          ANSI_BLACK_BACKGROUND  = "\u001B[40m";            // ANSI Background Color Codes
-    private static final String          ANSI_RED_BACKGROUND    = "\u001B[41m";
-    private static final String          ANSI_GREEN_BACKGROUND  = "\u001B[42m";
-    private static final String          ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    private static final String          ANSI_BLUE_BACKGROUND   = "\u001B[44m";
-    private static final String          ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    private static final String          ANSI_CYAN_BACKGROUND   = "\u001B[46m";
-    private static final String          ANSI_WHITE_BACKGROUND  = "\u001B[47m";
+    static final String                  ANSI_BLACK_BACKGROUND  = "\u001B[40m";            // ANSI Background Color Codes
+    static final String                  ANSI_RED_BACKGROUND    = "\u001B[41m";
+    static final String                  ANSI_GREEN_BACKGROUND  = "\u001B[42m";
+    static final String                  ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    static final String                  ANSI_BLUE_BACKGROUND   = "\u001B[44m";
+    static final String                  ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    static final String                  ANSI_CYAN_BACKGROUND   = "\u001B[46m";
+    static final String                  ANSI_WHITE_BACKGROUND  = "\u001B[47m";
 
-    private static Scanner               sc                     = new Scanner(System.in);  // Used for User Input
-    private static Random                rand                   = new Random();            // Random Numbers for CPU move selection
-    private static Pokemon               yourPoke               = new Pokemon();
-    private static Pokemon               opponentPoke           = new Pokemon();
-    private static int                   yourPick               = 0;
-    private static int                   opponentPick           = 0;
-    private static int                   decision               = 0;
-    private static int                   newYourPick            = 0;
-    private static int                   opponentDecision       = 0;
-    private static ArrayList<Pokemon>    poke                   = new ArrayList<Pokemon>();
+    static Scanner                       sc                     = new Scanner(System.in);  // Used for User Input
+    static Random                        rand                   = new Random();            // Random Numbers for CPU move selection
+    static Pokemon                       yourPoke               = new Pokemon();
+    static Pokemon                       opponentPoke           = new Pokemon();
+    static int                           yourPick               = 0;
+    static int                           opponentPick           = 0;
+    static int                           decision               = 0;
+//  static int                           newYourPick            = 0;
+    static int                           opponentDecision       = 0;
+    static ArrayList<Pokemon>            poke                   = new ArrayList<Pokemon>();
 
 
     /**
@@ -124,36 +124,17 @@ public class App {
         // BEGIN GAME WHILE LOOP
         while (yourPoke.getHealth() > 0.0 && opponentPoke.getHealth() > 0.0) {
 
-            // BEGIN PRE-FIGHT INFO UPDATE
+            Helper.infoUpdate();
+            
+            Helper.moveList();
 
-            System.out.println(ANSI_BLUE + yourPoke.getName() + " has " + yourPoke.getHealth() + " health left.");
-            System.out.println("The foe's " + opponentPoke.getName() + " has " + opponentPoke.getHealth() + " health left.\n");
+            Helper.yourMove();
 
-            // END PRE-FIGHT INFO UPDATE
+            Helper.healthCheck();
 
-            // BEGIN MOVE LIST
-            System.out.println(yourPoke.getName() + " knows\n1 - " + yourPoke.getMove1().getName() + "\t2 - "
-                               + yourPoke.getMove2().getName() + "\n3 - " + yourPoke.getMove3().getName() + "\t4 - "
-                               + yourPoke.getMove4().getName() + ANSI_GREEN);
-            System.out.print("Type the number corresponding to the move you would like " + yourPoke.getName()
-                             + " to use. Select '5' to switch out to a different Pokemon.\n>> ");
-            // END MOVE LIST
+            Helper.opponentMove();
 
-            // BEGIN YOUR MOVE
-            yourMove();
-            //END YOUR MOVE
-
-            // BEGIN HEALTH CHECKING BLOCK
-            healthCheck();
-            // END HEALTH CHECKING BLOCK
-
-            // BEGIN OPPONENT MOVE
-            opponentMove();
-            // END OPPONENT MOVE
-
-            // BEGIN HEALTH CHECKING BLOCK
-            healthCheck();
-            // END HEALTH CHECKING BLOCK
+            Helper.healthCheck();
 
         } // END GAME WHILE LOOP
 
@@ -162,99 +143,4 @@ public class App {
 
     } //END MAIN
 
-    // Begin yourMove subroutine
-    private static void yourMove() {
-        decision = sc.nextInt();
-
-        switch (decision) {
-            case 1:
-                Move.attack(yourPoke, opponentPoke, yourPoke.getMove1());
-                break;
-            case 2:
-                Move.attack(yourPoke, opponentPoke, yourPoke.getMove2());
-                break;
-            case 3:
-                Move.attack(yourPoke, opponentPoke, yourPoke.getMove3());
-                break;
-            case 4:
-                Move.attack(yourPoke, opponentPoke, yourPoke.getMove4());
-                break;
-            case 5:
-
-                for (Pokemon p : poke) {
-
-                    System.out.print(p.getName() + " is of type " + p.getType() + ". It has " + p.getHealth()
-                                     + " health points and it knows " + p.getMove1().getName() + ", " + p.getMove2().getName() + ", "
-                                     + p.getMove3().getName() + ", " + p.getMove4().getName() + ".\n");
-                }
-
-                System.out.print("\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.\n>> ");
-                int newYourPick = sc.nextInt();
-
-                if (newYourPick <= poke.size() && newYourPick > 0) {
-
-                    yourPoke = poke.get(newYourPick - 1);
-                    System.out.println("You have selected " + yourPoke.getName());
-                    System.out.println("Your opponent has selected " + opponentPoke.getName());
-
-                } else {
-
-                    sc.close();
-                    System.gc();
-                    throw new IllegalArgumentException("\n\n" + ANSI_RESET + ANSI_RED + "Please pick an integer between 1 and "
-                                                       + poke.size() + ", inclusive.\n" + ANSI_RESET);
-                }
-                break;
-
-            default:
-                throw new IllegalArgumentException("\n\n" + ANSI_RESET + ANSI_RED + "Please select an integer between 1 and 5, inclusive."
-                                                   + ANSI_RESET);
-        }
-    } //END yourMove subroutine
-
-    // BEGIN opponentMove subroutine
-    private static void opponentMove() {
-
-        opponentDecision = rand.nextInt(4);
-
-        System.out.println(ANSI_RED);
-        switch (opponentDecision) {
-            case 0:
-                Move.attack(opponentPoke, yourPoke, opponentPoke.getMove1());
-                System.out.println(opponentPoke.getName() + " used " + opponentPoke.getMove1().getName());
-                break;
-            case 1:
-                Move.attack(opponentPoke, yourPoke, opponentPoke.getMove2());
-                System.out.println(opponentPoke.getName() + " used " + opponentPoke.getMove2().getName());
-                break;
-            case 2:
-                Move.attack(opponentPoke, yourPoke, opponentPoke.getMove3());
-                System.out.println(opponentPoke.getName() + " used " + opponentPoke.getMove3().getName());
-                break;
-            case 3:
-                Move.attack(opponentPoke, yourPoke, opponentPoke.getMove4());
-                System.out.println(opponentPoke.getName() + " used " + opponentPoke.getMove4().getName());
-                break;
-        }
-        System.out.println(ANSI_GREEN);
-    } //END opponenetMove subroutine
-
-    //Begin Health chekcing subroutine
-    private static void healthCheck() {
-        if (yourPoke.getHealth() <= 0.0 && opponentPoke.getHealth() > 0.0) {
-
-            System.out.println("Sorry, but your opponent won.");
-            System.out.println(yourPoke.getName() + " has fainted.");
-            System.out.println(opponentPoke.getName() + " has " + opponentPoke.getHealth() + " health left.");
-            System.exit(0);
-
-        } else if (yourPoke.getHealth() > 0.0 && opponentPoke.getHealth() <= 0.0) {
-
-            System.out.println("Congratulations! You won!");
-            System.out.println("The foe's " + opponentPoke.getName() + " has fainted.");
-            System.out.println(yourPoke.getName() + " has " + yourPoke.getHealth() + " health left.");
-            System.exit(0);
-
-        }
-    } // END health checking subroutine
 }
---
+++ src/main/java/me/pavva/PokeJava/Helper.java
@@ -0,0 +1,136 @@
+package me.pavva.PokeJava;
+
+/**
+ * A helper class which contains all the game logic. Setup logic is preserved 
+ * in {@link me.pavva.PokeJava.App App}. Variables are statically referenced
+ * to {@link me.pavva.PokeJava.App App}.
+ * 
+ * @see me.pavva.PokeJava.App
+ */
+public class Helper {
+    
+    /**
+     * A static method which contains the code for the user's game turn.
+     */
+    static void yourMove() {
+        App.decision = App.sc.nextInt();
+
+        switch (App.decision) {
+            case 1:
+                Move.attack(App.yourPoke, App.opponentPoke, App.yourPoke.getMove1());
+                break;
+            case 2:
+                Move.attack(App.yourPoke, App.opponentPoke, App.yourPoke.getMove2());
+                break;
+            case 3:
+                Move.attack(App.yourPoke, App.opponentPoke, App.yourPoke.getMove3());
+                break;
+            case 4:
+                Move.attack(App.yourPoke, App.opponentPoke, App.yourPoke.getMove4());
+                break;
+            case 5:
+
+                for (Pokemon p : App.poke) {
+
+                    System.out.print(p.getName() + " is of type " + p.getType() + ". It has " + p.getHealth()
+                                     + " health points and it knows " + p.getMove1().getName() + ", " + p.getMove2().getName() + ", "
+                                     + p.getMove3().getName() + ", " + p.getMove4().getName() + ".\n");
+                }
+
+                System.out.print("\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.\n>> ");
+                int newYourPick = App.sc.nextInt();
+
+                if (newYourPick <= App.poke.size() && newYourPick > 0) {
+
+                    App.yourPoke = App.poke.get(newYourPick - 1);
+                    System.out.println("You have selected " + App.yourPoke.getName());
+                    System.out.println("Your opponent has selected " + App.opponentPoke.getName());
+
+                } else {
+
+                    App.sc.close();
+                    System.gc();
+                    throw new IllegalArgumentException("\n\n" + App.ANSI_RESET + App.ANSI_RED + "Please pick an integer between 1 and "
+                                                       + App.poke.size() + ", inclusive.\n" + App.ANSI_RESET);
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException("\n\n" + App.ANSI_RESET + App.ANSI_RED
+                                                   + "Please select an integer between 1 and 5, inclusive." + App.ANSI_RESET);
+        }
+    }
+    
+    
+    /**
+     * A static method which contains the code for the CPU's game turn
+     */
+    static void opponentMove() {
+
+        App.opponentDecision = App.rand.nextInt(4);
+
+        System.out.println(App.ANSI_RED);
+        switch (App.opponentDecision) {
+            case 0:
+                Move.attack(App.opponentPoke, App.yourPoke, App.opponentPoke.getMove1());
+                System.out.println(App.opponentPoke.getName() + " used " + App.opponentPoke.getMove1().getName());
+                break;
+            case 1:
+                Move.attack(App.opponentPoke, App.yourPoke, App.opponentPoke.getMove2());
+                System.out.println(App.opponentPoke.getName() + " used " + App.opponentPoke.getMove2().getName());
+                break;
+            case 2:
+                Move.attack(App.opponentPoke, App.yourPoke, App.opponentPoke.getMove3());
+                System.out.println(App.opponentPoke.getName() + " used " + App.opponentPoke.getMove3().getName());
+                break;
+            case 3:
+                Move.attack(App.opponentPoke, App.yourPoke, App.opponentPoke.getMove4());
+                System.out.println(App.opponentPoke.getName() + " used " + App.opponentPoke.getMove4().getName());
+                break;
+        }
+        System.out.println(App.ANSI_GREEN);
+    }
+    
+    /**
+     * A static method which checks the health value of each pokemon to determine the winner.
+     * This is done by checking if a pokemon's health is at or below zero, whilst the other's 
+     * health value is above 0. If neither pokemon's health value is at or below 0, the game
+     * continues.
+     */
+    static void healthCheck() {
+        if (App.yourPoke.getHealth() <= 0.0 && App.opponentPoke.getHealth() > 0.0) {
+
+            System.out.println("Sorry, but your opponent won.");
+            System.out.println(App.yourPoke.getName() + " has fainted.");
+            System.out.println(App.opponentPoke.getName() + " has " + App.opponentPoke.getHealth() + " health left.");
+            System.exit(0);
+
+        } else if (App.yourPoke.getHealth() > 0.0 && App.opponentPoke.getHealth() <= 0.0) {
+
+            System.out.println("Congratulations! You won!");
+            System.out.println("The foe's " + App.opponentPoke.getName() + " has fainted.");
+            System.out.println(App.yourPoke.getName() + " has " + App.yourPoke.getHealth() + " health left.");
+            System.exit(0);
+
+        }
+    }
+
+    /**
+     * A static method which lists the moves yourPoke known. Used at the beginning of each turn
+     */
+    static void moveList() {
+        System.out.println(App.yourPoke.getName() + " knows\n1 - " + App.yourPoke.getMove1().getName() + "\t2 - "
+                           + App.yourPoke.getMove2().getName() + "\n3 - " + App.yourPoke.getMove3().getName() + "\t4 - "
+                           + App.yourPoke.getMove4().getName() + App.ANSI_GREEN);
+        System.out.print("Type the number corresponding to the move you would like " + App.yourPoke.getName()
+                         + " to use. Select '5' to switch out to a different Pokemon.\n>> ");
+    }
+    
+    /**
+     * Lists the amount of health each pokemon has left. Used at the beginning of each turn.
+     */
+    static void infoUpdate() {
+        System.out.println(App.ANSI_BLUE + App.yourPoke.getName() + " has " + App.yourPoke.getHealth() + " health left.");
+        System.out.println("The foe's " + App.opponentPoke.getName() + " has " + App.opponentPoke.getHealth() + " health left.\n");
+    }
+}
