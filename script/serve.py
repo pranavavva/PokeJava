c22a163df25cc3dede9ad007bec8b8438f16a921
Pranav A <pranav.avva@gmail.com>
2017-12-27 20:00:33 -0500

    buggy

--- script/serve.py
+++ script/serve.py
@@ -4,15 +4,10 @@ import os
 from os import system
 import sys
 
-want = str(input("This script requires sudo. Do you have superuser perms? YES or NO")
+print("==> Updating")
+system("script/update.py")
 
-if want.lower() == "yes":
-	print("==> Updating")
-	system("script/update")
-
-	print("==> Snapcrafting")
-	system("snapcraft")
-	system("snap install PokeJavaApp-4.1.1.snap")
-	system("sudo pokejava")
-else:
-	sys.exit(0)
+print("==> Snapcrafting")
+system("snapcraft")
+system("snap install PokeJavaApp-4.1.1.snap")
+system("sudo pokejava")
