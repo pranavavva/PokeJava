4aa6c8866edb2d91741eb6da8ce203bf654ae19c
Pranav A <pranav.avva@gmail.com>
2017-12-27 19:46:01 -0500

    added a couple scripts

---
+++ script/bootstrap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from os import system as sys
+import os
+
+if (os.path.isfile('./pom.xml')):
+	sys("mvn dependency:resolve")
+
+print()
+print("Dependencies Installed")
---
+++ script/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import os
+from os import system
+import sys
+
+while True:
+	print("This script is to be used only to return the working directory to an initial clone state. Is this what you want? YOUR CHANGES WILL BE STASHED AND BRANCH MASTER WILl BE CHECKED OUT")
+
+	want_str = str(input("Yes or No?"))
+	if want_str.lower() == yes:
+		want = True
+		break
+	elif want_str.lower() == no:
+		want = False
+		sys.exit(0)
+	else:
+		print("That's not an answer. Enter YES or NO")
+
+print("==> stashing changes")
+system("git stash")
+
+print("==> checking out to master")
+system("git checkout master")
+
+print("==> Bootstrapping")
+system("script/bootstrap.py")
+
+print("==> Working directory cleaned, branch master checked out, dependencies installed.")
