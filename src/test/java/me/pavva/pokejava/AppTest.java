118853cd3a49472eb855d9a348e623d29993ddaf
Pranav A <pranav.avva@gmail.com>
2018-02-28 13:33:26 +0000

    styuff

---
+++ src/test/java/me/pavva/pokejava/AppTest.java
@@ -0,0 +1,14 @@
+package me.pavva.pokejava;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
+    App app = new App();
+    
+    // @Test
+    public void testMain() {
+        app.main(new String[]{});v 
+    }
+}
