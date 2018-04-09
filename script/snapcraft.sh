db5371093c45c52c6833d74601141414d7e7b110
Pranav A <pranav.avva@gmail.com>
2018-04-09 14:46:25 +0000

    no more snaps

--- script/snapcraft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-snapcraft clean pokejava
-snapcraft
-sudo snap install pokejava_*_amd64.snap --devmode --dangerous
-
-echo '==> pokejava is ready to run. Use `sudo pokejava` to run'
