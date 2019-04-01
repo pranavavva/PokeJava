9f5b3abaccd3a6b3b086f45f161319b6b5e6f1c0
Pranav A <pranav.avva@gmail.com>
2019-04-01 08:39:55 -0400

    Use printf() to make pokemon listing code cleaner

--- src/main/java/me/pavva/pokejava/App.java
+++ src/main/java/me/pavva/pokejava/App.java
@@ -4,13 +4,14 @@ import java.util.*;
 
 import me.pavva.pokejava.pokemon.*;
 
-
 /**
  * Entrypoint to Pokemon Battle Simulator.
  * 
- * Contains game setup. Uses subroutines in {@link me.pavva.pokejava.Helper Helper} to seperate setup from logic.
+ * Contains game setup. Uses subroutines in {@link me.pavva.pokejava.Helper
+ * Helper} to seperate setup from logic.
  *
- * @author Pranav Avva Github: 20avva Web: <a href="https://20avva.github.io">20avva.github.io</a>
+ * @author Pranav Avva Github: 20avva Web:
+ *         <a href="https://20avva.github.io">20avva.github.io</a>
  * @version 4.1
  * @see me.pavva.pokejava.Pokemon
  * @see me.pavva.pokejava.Type
@@ -19,57 +20,60 @@ import me.pavva.pokejava.pokemon.*;
  * @since Java 1.7
  */
 public class App {
-    
+
     /**
      * Scanner for user input
      */
-    static Scanner            sc               = new Scanner(System.in);  // Used for User Input
-    
+    static Scanner sc = new Scanner(System.in); // Used for User Input
+
     /**
      * Random number generator, used for computer move and pokemon selection.
      */
-    static Random             rand             = new Random();            // Random Numbers for CPU move selection
-    
+    static Random rand = new Random(); // Random Numbers for CPU move selection
+
     /**
      * The user's pokemon
      * 
      * @see Pokemon
      */
-    static Pokemon            yourPoke         = new Pokemon();
-    
+    static Pokemon yourPoke = new Pokemon();
+
     /**
      * The opponent's (computer's) pokemon
      * 
      * @see Pokemon
      */
-    static Pokemon            opponentPoke     = new Pokemon();
-    
+    static Pokemon opponentPoke = new Pokemon();
+
     /**
      * A temporary variable to store the user's move choice
      */
-    static int                decision         = 0;
-    
+    static int decision = 0;
+
     /**
      * A temporary vaiable to store the opponent's move choice
      */
-    static int                opponentDecision = 0;
-    
+    static int opponentDecision = 0;
+
     /**
-     * An ArrayList to hold all the pokemon from which the user and the computer can pick pokemon from
+     * An ArrayList to hold all the pokemon from which the user and the computer can
+     * pick pokemon from
      * 
      * @see Pokemon
      */
-    static ArrayList<Pokemon> poke             = new ArrayList<Pokemon>();
-
+    static ArrayList<Pokemon> poke = new ArrayList<Pokemon>();
 
     /**
-     * Main method in the Pokemon Battle Simulator.<br><br>
+     * Main method in the Pokemon Battle Simulator.<br>
+     * <br>
      * 
-     * <code>main()</code> invokes methods from {@link Helper Helper} for the main game loop. This method, 
-     * however, adds all the pokemon to a master list, which is then presented to the user with the 
-     * {@link Pokemon Pokemon} that they can choose, after whom the computer selects a pokemon. 
+     * <code>main()</code> invokes methods from {@link Helper Helper} for the main
+     * game loop. This method, however, adds all the pokemon to a master list, which
+     * is then presented to the user with the {@link Pokemon Pokemon} that they can
+     * choose, after whom the computer selects a pokemon.
      *
-     * @param args A string array created from command-line arguments. Not used in this program.
+     * @param args A string array created from command-line arguments. Not used in
+     *             this program.
      */
     public static void main(String[] args) {
 
@@ -100,15 +104,15 @@ public class App {
         int k = 1;
         // BEGIN POKEMON SELECTION BLOCK
         for (Pokemon p : poke) {
+            System.out.printf("%s - %s is of type %s. It has %s health points and it knows %s, %s, %s, %s.\n",
+                    Integer.toString(k), p.getName(), p.getType().toString(), Double.toString(p.getHealth()),
+                    p.getMove1().getName(), p.getMove2().getName(), p.getMove3().getName(), p.getMove4().getName());
 
-            System.out.print(k + " - " + p.getName() + " is of type " + p.getType() + ". It has " + p.getHealth()
-                             + " health points and it knows " + p.getMove1().getName() + ", " + p.getMove2().getName() + ", "
-                             + p.getMove3().getName() + ", " + p.getMove4().getName() + ".\n");
             k++;
         }
 
-
-        System.out.print("\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.\n>> ");
+        System.out.print(
+                "\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.\n>> ");
         yourPick = sc.nextInt();
         System.out.println("\n");
         opponentPick = rand.nextInt(poke.size());
@@ -123,7 +127,8 @@ public class App {
         } else {
 
             sc.close();
-            throw new IllegalArgumentException("\n\n" + "Please pick an integer between 1 and " + poke.size() + ", inclusive.\n");
+            throw new IllegalArgumentException(
+                    "\n\n" + "Please pick an integer between 1 and " + poke.size() + ", inclusive.\n");
         }
         // END POKEMON SELECTION BLOCK
 
@@ -143,11 +148,10 @@ public class App {
             Helper.healthCheck();
 
         } // END GAME WHILE LOOP
-        
+
         sc.close();
         System.exit(0);
 
-    } //END MAIN METHOD
+    } // END MAIN METHOD
 
 } // END APP.JAVA
-
